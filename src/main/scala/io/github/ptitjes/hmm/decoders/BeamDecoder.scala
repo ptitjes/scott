b2e65648e02f0f1230875ce6a5b44854cbb3c5da
Didier Villevalois <ptitjes@free.fr>
2014-12-03 16:02:22 +0100

    Factor out common data structures

--- src/main/scala/io/github/ptitjes/hmm/decoders/BeamDecoder.scala
+++ src/main/scala/io/github/ptitjes/hmm/decoders/BeamDecoder.scala
@@ -3,12 +3,11 @@ package io.github.ptitjes.hmm.decoders
 import io.github.ptitjes.hmm.Corpora._
 import io.github.ptitjes.hmm.Utils._
 import io.github.ptitjes.hmm._
+import io.github.ptitjes.hmm.decoders.arrays._
 import io.github.ptitjes.hmm.utils.BoundedPriorityQueue
 
 import scala.annotation.tailrec
-import scala.collection._
-import scala.collection.mutable
-import scala.reflect.ClassTag
+import scala.collection.{mutable, _}
 
 object BeamDecoder extends Decoder.Factory {
 
@@ -64,13 +63,9 @@ object BeamDecoder extends Decoder.Factory {
 
 			var sharedTagsCount = 1
 			val sharedTagsFanning = breadth
-			var sharedTags = makeRange(sharedTagsCount)
-
-			var allStatesCount = sourceTagsCount * sharedTagsCount
-			var allStates = makeRange(allStatesCount)
+			var sharedTags = 0 until sharedTagsCount
 
 			val targetTagsCount = breadth
-			val targetTags = makeRange(targetTagsCount)
 
 			val iterator = sequence.iterator(breadth, depth)
 			while (iterator.hasNext) {
@@ -142,10 +137,7 @@ object BeamDecoder extends Decoder.Factory {
 
 						sharedTagsCount = pow(breadth, d)
 					}
-					sharedTags = makeRange(sharedTagsCount)
-
-					allStatesCount = sourceTagsCount * sharedTagsCount
-					allStates = makeRange(allStatesCount)
+					sharedTags = 0 until sharedTagsCount
 				}
 
 				deltas.swap()
@@ -173,10 +165,6 @@ object BeamDecoder extends Decoder.Factory {
 			AnnotatedSequence(sequence.observables.zip(states))
 		}
 
-		def makeRange(count: Int): GenSeq[Int] = {
-			if (multiThreaded) (0 until count).par else 0 until count
-		}
-
 		@inline def maxArgMax(count: Int, beam: mutable.BitSet,
 		                      arg: Int => Int, f: Int => Double): (Double, Int) = {
 			var max = Double.NegativeInfinity
@@ -198,39 +186,6 @@ object BeamDecoder extends Decoder.Factory {
 
 			(max, argMax)
 		}
-
-		final class SwappableArray[T: ClassTag](size: Int) {
-			private var current: Array[T] = new Array[T](size)
-			private var last: Array[T] = new Array[T](size)
-
-			def apply(i: Int): T = last(i)
-
-			def update(i: Int, v: T) = current(i) = v
-
-			def swap(): Unit = {
-				val temp = current
-				current = last
-				last = temp
-			}
-		}
-
-		final class PsiArray(size: Int, length: Int) {
-			private val data = Array.ofDim[Int](length, size)
-			private var index = 0
-
-			def apply(i: Int): Int = data(index - 1)(i)
-
-			def update(i: Int, v: Int) = data(index)(i) = v
-
-			def forward(): Unit = index = index + 1
-
-			def backward(): Unit = index = index - 1
-
-			def isRewound: Boolean = index == 0
-
-			def rewind(): Unit = index = 0
-		}
-
 	}
 
 }
